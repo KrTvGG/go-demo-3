cabae0f767d50e2d46b9b25ab9e54def2d9ba28b
KrTv <krtviv@mail.ru>
2026-02-08 10:31:00 +0300

    7.3. Итерация по Map

--- main.go
+++ main.go
@@ -3,19 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	m := map[string]string {
-		"PurpleShcool": "purpleschool.ru",
+	m := map[string]int{"a": 1, "b": 2}
+	for key, value := range m {
+		fmt.Println(key, value)
 	}
-
-	fmt.Println(m)
-	fmt.Println(m["PurpleShcool"])
-	m["PurpleShcool"] = "https://purpleschool.ru"
-	fmt.Println(m)
-	m["Google"] = "https://google.com"
-	m["Yandex"] = "https://yandex.ru"
-	fmt.Println(m)
-	delete(m, "Yandex")
-	delete(m, "Y")
-	fmt.Println(m["Y"])
-	fmt.Println(m)
 }
