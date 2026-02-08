5ffca8193efc43358433eaf32cc1b8ae52f6e6b2
KrTv <krtviv@mail.ru>
2026-02-08 10:24:08 +0300

    7.2. Изменение Map

--- main.go
+++ main.go
@@ -4,8 +4,18 @@ import "fmt"
 
 func main() {
 	m := map[string]string {
-		"PurpleShcool": "https://purpleschool.ru",
+		"PurpleShcool": "purpleschool.ru",
 	}
 
+	fmt.Println(m)
+	fmt.Println(m["PurpleShcool"])
+	m["PurpleShcool"] = "https://purpleschool.ru"
+	fmt.Println(m)
+	m["Google"] = "https://google.com"
+	m["Yandex"] = "https://yandex.ru"
+	fmt.Println(m)
+	delete(m, "Yandex")
+	delete(m, "Y")
+	fmt.Println(m["Y"])
 	fmt.Println(m)
 }
