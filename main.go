be2d65fc72ab69ac8182f9c827ff02d646ed733a
KrTv <krtviv@mail.ru>
2026-02-09 21:43:54 +0300

    7.4. Упражнение - Утилита закладок

--- main.go
+++ main.go
@@ -3,8 +3,58 @@ package main
 import "fmt"
 
 func main() {
-	m := map[string]int{"a": 1, "b": 2}
-	for key, value := range m {
-		fmt.Println(key, value)
+	fmt.Println("__Заметки__")
+	bookmarks := map[string]string{}
+Menu:
+	for {
+		numMenu := menu()
+
+		switch numMenu {
+		case 1:
+			printBookmarks(bookmarks)
+		case 2:
+			addBookmark(bookmarks)
+		case 3:
+			deleteBookmark(bookmarks)
+		default:
+			break Menu
+		}
 	}
+}
+
+func deleteBookmark(bookmarks map[string]string) {
+	var name string
+	fmt.Print("Ввыдите имя закладки: ")
+	fmt.Scan(&name)
+	delete(bookmarks, name)
+}
+
+func addBookmark(bookmarks map[string]string) {
+	var name, text string
+	fmt.Print("Введите имя закладки: ")
+	fmt.Scan(&name)
+	fmt.Print("Введите текст закладки: ")
+	fmt.Scan(&text)
+	bookmarks[name] = text
+}
+
+func printBookmarks(bookmarks map[string]string) {
+	if len(bookmarks) == 0 {
+		fmt.Println("Закладки пустые")
+	}
+	for name, desc := range bookmarks {
+		fmt.Printf("%v: %v\n", name, desc)
+	}
+}
+
+func menu() int {
+	var num int
+	fmt.Println("Выберите вариант")
+	fmt.Println("1. Посмотреть закладку")
+	fmt.Println("2. Добавить закладку")
+	fmt.Println("3. Удалить закладку")
+	fmt.Println("4. Выход")
+	fmt.Print("> ")
+	fmt.Scan(&num)
+	return num
 }
